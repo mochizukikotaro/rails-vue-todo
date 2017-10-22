909621f2253d5f6c3a8257a490d7bf5ae9bbefb6
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-10-22 16:53:55 +0900

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+tasks = [
+  "Cooking",
+  "Cleaning",
+  "Watching TV"
+]
+
+Task.destroy_all
+tasks.each do |name|
+  Task.create(name: name)
+end
